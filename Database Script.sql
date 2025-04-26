312e1938e0e205f827e8b414c6ae9e2d0dea00aa
Muhammad Qaisar <engr.qahmad@gmail.com>
2025-04-26 12:10:36 +0500

    Updated DB Commands

--- Database Script.sql
+++ Database Script.sql
@@ -9,17 +9,18 @@ alter table users add primary key (username, api_key);
 truncate users;
 
 insert into users (username, password, first_name, last_name, email, phone_number, last_login, api_key) 
-values ('amansingh', md5('password here'), 'Aman', 'Singh', 'singhaman11415@gmail.com', '8770262013', now(), 'abhikuchnhihai');
+values ('QaisarAhmad', md5('put ur pass'), 'Muhammad', 'Qaisar', 'engr.qahmad@gmail.com', '030000', now(), 'not-created-yet');
+
 
 create table Node (deviceID varchar(255), username varchar(255), field_name varchar(255), temperature int, humidity int, moisture int, light int, 
 foreign key (username) references users(username), primary key (deviceID));
 select * from Node;
-select field_name, temperature, humidity, moisture, light from Node where username="amansingh";
+select field_name, temperature, humidity, moisture, light from Node where username="QaisarAhmad";
 
 insert into Node (deviceID, username, field_name, temperature, humidity, moisture, light)
-values('ARMS12012', 'amansingh', 'Rose Garden', 45, 54, 100, 600);
-insert into Node (deviceID, username, field_name, temperature, humidity, moisture, light)
-values('ARMS22212', 'hellboy', 'Samy Garden', 45, 54, 100, 600);
+values('ARMS12012', 'QaisarAhmad', 'Rose Garden', 45, 54, 100, 600);
+-- insert into Node (deviceID, username, field_name, temperature, humidity, moisture, light)
+-- values('ARMS22212', 'hellboy', 'Samy Garden', 45, 54, 100, 600);
 
 select * from Node;
 
@@ -29,10 +30,12 @@ foreign key (deviceID) references Node(deviceID));
 insert into Rosegarden (deviceID, temperature, humidity, moisture, light, date_time)
 values('ARMS12012', 45, 54, 100, 600, now());
 
-select * from users where username = "hellboy";
-update users set last_login = now() where username = "hellboy";
+-- select * from users where username = "hellboy";
+-- update users set last_login = now() where username = "hellboy";
 
 select * from Rosegarden;
 select * from (select * from Rosegarden order by date_time desc limit 10) dummy order by date_time asc;
 select * from (select * from Rosegarden order by date_time desc limit 10) dummy order by date_time asc;
+
+
 
